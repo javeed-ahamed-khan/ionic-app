81f53ccbb372d7878628350ae3f2205bc76f2f7d
javeedahamedkhan <javeedahamedkhan@xiaomi.com>
2023-03-18 17:12:31 +0530

    feat: virtuso added

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,35 +1,36 @@
-import { IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonContent,IonHeader, IonInfiniteScroll, IonInfiniteScrollContent, IonList, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
 import { useAppDispatch, useAppSelector } from '../redux/hooks';
 import { getPosts, IPost } from '../redux/postSlice';
+import { Virtuoso } from 'react-virtuoso'
 
 const Dashboard: React.FC = () => {
   const [items, setItems] = useState([] as IPost[]);
-  const {posts} = useAppSelector(state=>state.posts)
+  const { posts } = useAppSelector(state => state.posts)
   const history = useHistory()
   const dispatch = useAppDispatch()
 
-  const generateItems = () => {
-    const newItems = [];
-    for (let i = 0; i < 10 && items.length<posts.length; i++) {
-      newItems.push(posts[i+items.length]);
-    }
-    setItems([...items, ...newItems]);
-  };
+  // const generateItems = () => {
+  //   const newItems = [];
+  //   for (let i = 0; i < 10 && items.length < posts.length; i++) {
+  //     newItems.push(posts[i + items.length]);
+  //   }
+  //   setItems([...items, ...newItems]);
+  // };
 
-  useEffect(()=>{
+  useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts')
-  .then((response) => response.json())
-  .then((json) => dispatch(getPosts(json)))
-  },[dispatch])
+      .then((response) => response.json())
+      .then((json) => dispatch(getPosts(json)))
+  }, [dispatch])
 
-  useEffect(()=>{
-    generateItems()
-    // eslint-disable-next-line
-  },[posts])
+  // useEffect(() => {
+  //   generateItems()
+  //   // eslint-disable-next-line
+  // }, [posts])
 
-  const handleClick=(id:number)=>{
+  const handleClick = (id: number) => {
     history.push(`/page/detail/${id}`)
   }
   console.log("re-render")
@@ -45,29 +46,20 @@ const Dashboard: React.FC = () => {
       </IonHeader>
 
       <IonContent fullscreen>
-        <IonList>
-          {items.map((item) => (
-                  <IonCard key={item.id} onClick={()=>handleClick(item.id)}>
-                  <IonCardHeader>
-                    <IonCardTitle>{item.title}</IonCardTitle>
-                  </IonCardHeader>
-                  <IonCardContent>
-                  {item.body}
-                  </IonCardContent>
-                </IonCard>
-                ))}
-        </IonList>
-        <IonInfiniteScroll
-        onIonInfinite={(ev) => {
-          // generateItems();
-          setTimeout(async() =>{ 
-            await generateItems();
-            ev.target.complete()
-          }, 1000);
-        }}
-      >
-        <IonInfiniteScrollContent></IonInfiniteScrollContent>
-      </IonInfiniteScroll>
+          <Virtuoso
+            style={{ height: '100%' }}
+            totalCount={posts.length}
+            data={posts}
+            itemContent={(index, post) => (
+              <IonCard key={post.id} onClick={() => handleClick(post.id)}>
+                <IonCardHeader>
+                  <IonCardTitle>{post.title}</IonCardTitle>
+                </IonCardHeader>
+                <IonCardContent>
+                  {post.body}
+                </IonCardContent>
+              </IonCard>)}
+          />
       </IonContent>
     </IonPage>
   );
